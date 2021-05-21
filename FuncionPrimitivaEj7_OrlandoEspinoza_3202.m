77eecff8a3d51d3af35b71249cff2a53ab98ba57
Orlando-Esp <82452841+Orlando-Esp@users.noreply.github.com>
2021-05-20 21:15:07 -0500

    Update FuncionPrimitivaEj7_OrlandoEspinoza_3202.m

--- FuncionPrimitivaEj7_OrlandoEspinoza_3202.m
+++ FuncionPrimitivaEj7_OrlandoEspinoza_3202.m
@@ -1,10 +1,10 @@
 % Octave Script
-% Title			:Funcion Primitiva ej1.
-% Description		:Script para desarrrollar el ejericio1 de la actividad Funcion Primitiva.
+% Title			:Funcion Primitiva ej7.
+% Description		:Script para desarrrollar el ejericio7 de la actividad Funcion Primitiva.
 % Author		:Orlando Espinoza Valentin (Orlando-Esp) espinozaorlando978@gmail.com
 % Date			:20210520
 % sion		        :1
-% Usage			:octave> /path/Sumas de Riemann,FuncionPrimitivaEj1_OrlandoEspinoza_3202.m
+% Usage			:octave> /path/Sumas de Riemann,FuncionPrimitivaEj7_OrlandoEspinoza_3202.m
 % Notes			:Requiere aplicacion octave-online usar en consola preferentemente. 
 %program execution page :https://octave-online.net
 
