1b1eef5392aa9d6ec68a1b6e40a22dff5ab362cb
Orlando-Esp <82452841+Orlando-Esp@users.noreply.github.com>
2021-05-20 21:46:57 -0500

    Create FuncionPrimitivaEj13_OrlandoEspinoza_3202.m

---
+++ FuncionPrimitivaEj13_OrlandoEspinoza_3202.m
@@ -0,0 +1,17 @@
+% Octave Script
+% Title			:Funcion Primitiva ej13.
+% Description		:Script para desarrrollar el ejericio13 de la actividad Funcion Primitiva.
+% Author		:Orlando Espinoza Valentin (Orlando-Esp) espinozaorlando978@gmail.com
+% Date			:20210520
+% sion		        :1
+% Usage			:octave> /path/Sumas de Riemann,FuncionPrimitivaEj13_OrlandoEspinoza_3202.m
+% Notes			:Requiere aplicacion octave-online usar en consola preferentemente. 
+%program execution page :https://octave-online.net
+
+%paqueteria symbolic.
+pkg load symbolic
+%Crea y da un valor inicial a una variable.
+syms x;
+%Crea y da un valor inicial a una variable.
+int (3*x^4) 
+ans = (sym)
